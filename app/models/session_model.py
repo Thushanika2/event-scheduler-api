4f02b790ff41e455b24a2d4b153c1abcb0a992c8
Shanujan <shanushanujan234@gmail.com>
2026-06-30 09:13:27 +0530

    session model created

---
+++ app/models/session_model.py
@@ -0,0 +1,29 @@
+from app.extensions import db
+from app.utils import utc_now
+
+class Session(db.Model):
+    __tablename__="sessions"
+    session_id = db.Column( db.Integer, primary_key=True, autoincrement=True )
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+    title = db.Column( db.String, nullable=False )
+    speaker = db.Column( db.String, nullable=False )
+    date = db.Column( db.Date, nullable=False )
+    start_time = db.Column( db.Time, nullable=False )
+    end_time = db.Column( db.Time, nullable=False )
+    capacity = db.Column( db.Integer, nullable=False )
+
+    def to_dict(self):
+        return {
+            "session_id": self.session_id,
+            "room_id": self.room_id,
+            "title": self.title,
+            "speaker": self.speaker,
+            "date": self.date,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "capacity": self.capacity
+        }
+
+
+
+
