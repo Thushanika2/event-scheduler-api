5c2087f9708a3d53087e4c86a9ac0474dab41e91
Shanujan <shanushanujan234@gmail.com>
2026-06-30 13:55:38 +0530

    user id and agenda model updated

--- app/models/agenda_model.py
+++ app/models/agenda_model.py
@@ -4,8 +4,8 @@ class Agenda(db.Model):
     __tablename__ = "agendas"
 
     agenda_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    session_id = db.Column(db.Integer, db.ForeignKey('session.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    session_id = db.Column(db.Integer, db.ForeignKey('sessions.session_id'))
 
      
 
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     __tablename__ = "users"
 
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
     email=db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), nullable=False, default="user")
