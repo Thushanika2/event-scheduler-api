ee3d55ae2a6e1ec28959859896ab20e3afab38f1
Shanujan <shanushanujan234@gmail.com>
2026-06-30 13:25:39 +0530

    routes init file updated

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,14 @@
 from app.routes.auth_routes import auth_bp
+from app.routes.session_routes import session_bp
+from app.routes.room_routes import room_bp
+from app.routes.agenda_routes import agenda_bp
 
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp)
+    app.register_blueprint(session_bp)
+    app.register_blueprint(room_bp)
+    app.register_blueprint(agenda_bp)
+
+
 
