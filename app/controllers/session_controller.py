69210d54d55e650e9cdc6ccea217dda726625c6d
Shanujan <shanushanujan234@gmail.com>
2026-06-30 10:29:23 +0530

    session_update completely

--- app/controllers/session_controller.py
+++ app/controllers/session_controller.py
@@ -33,3 +33,27 @@ def get_session(session_id):
         return jsonify({"error": "Session not found."}), 404
     return jsonify({"session": session.to_dict()}), 200
 
+def update_session(session_id):
+    session = Session.query.get(session_id)
+    if not session:
+        return jsonify({"error": "Session not found."}), 404
+    
+    data = request.get_json(silent=True)
+    if not data:
+        return jsonify({"error": "No data provided to update."}), 400
+    
+    try:
+        session.room_id = data.get("room_id")
+        session.title = data.get("title")
+        session.speaker = data.get("speaker")
+        session.date = data.get("date")
+        session.start_time = data.get("start_time")
+        session.end_time = data.get("end_time")
+        session.capacity = data.get("capacity")
+        db.session.commit()
+        return jsonify ({"message":"Session update successfully ", "session":session.to_dict()}),200
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": e})
+    
